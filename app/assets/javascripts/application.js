c9b4e5681c6aa53d16d6055fefbad8d12d8bacd1
ars-ishi <ars.ishi11@gmail.com>
2018-09-18 17:47:52 +0900

    Edit application.js
    グループ内の最新メッセージが表示される部分までスクロールするfunctionを追記

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,17 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+    var timeline = $('.chat-area__timeline');
+    $(document).ready(function() {
+       $(timeline).delay(5000).animate({
+          scrollTop: $(timeline).height()
+        }, 3000);
+    });
+    $(timeline).on('scroll', function() {
+       $(timeline).delay(5000).animate({
+          scrollTop: $(timeline).height()
+        }, 3000);
+    });
+});
