b41eb5c009626797ff2087acdd922d404082e52c
ars-ishi <ars.ishi11@gmail.com>
2018-09-20 19:11:13 +0900

    Edit models/user, models/group, models/group_user
    アソシエーションのスペルミスと、記述の順序を変更

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
-  has_many :messages
-  has_many :users, through: :group_users
   has_many :group_users
-  validates :group_name, presence: true  
+  has_many :users, through: :group_users
+  validates :group_name, presence: true
 end
