a55afcdb134767db3f9f93d7f79b7e4aa36dc3a5
ars-ishi <ars.ishi11@gmail.com>
2018-09-27 18:02:54 +0900

    Edit javascripts/message.js
    ・インデントを調整
    ・メッセージの自動更新機能を追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function() {
   function buildHTML(message){
     var image = message.image ? `<img src="${message.image}">` : "";
-    var html =  `<li>
+    var html =  `<li class="messages" data-id="${message.id}">
                   <div class="message-list__name">${message.user_name}</div>
                   <div class="message-list__date">${message.date}</div>
                   <div class="message-list__text">${message.content}</div>
@@ -15,29 +15,53 @@ $(function() {
     var url = $(this).attr('action');
     $('.form__submit__btn').removeAttr('data-disable-with');
     $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
     .done(function(data){
-        var html = buildHTML(data);
-        $('.timeline__message-list').append(html);
-        $('#new_message')[0].reset();
-        $('.main').prepend(`<div class="flash-message-notice">メッセージを送信しました</div>`);
-        $(".flash-message-notice").fadeOut(4000).queue(function() {
-            this.remove();
-        });
-        $('.chat-area__timeline').animate({
-            scrollTop: $('.chat-area__timeline').get(0).scrollHeight}, 4000);
-      })
+      var html = buildHTML(data);
+      $('.timeline__message-list').append(html);
+      $('#new_message')[0].reset();
+      $('.main').prepend(`<div class="flash-message-notice">メッセージを送信しました</div>`);
+      $(".flash-message-notice").fadeOut(4000).queue(function() {
+          this.remove();
+      });
+      $('.chat-area__timeline').animate({
+          scrollTop: $('.chat-area__timeline').get(0).scrollHeight}, 4000);
+    })
     .fail(function(){
-        $('.main').prepend(`<div class="flash-message-alert">送信できませんでした</div>`);
-        $(".flash-message-alert").fadeOut(4000).queue(function() {
-            this.remove();
-        });
+      $('.main').prepend(`<div class="flash-message-alert">送信できませんでした</div>`);
+      $(".flash-message-alert").fadeOut(4000).queue(function() {
+          this.remove();
+      });
     })
   })
+  $(function(){
+    setInterval(update, 5000);
+  });
+  function update(){
+    if( $('.messages')[0] ) {
+      var message_id = $('.messages:last').data('id');
+    }
+    else {
+      var message_id = 0
+    }
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: { message: { id: message_id } },
+      dataType: 'json'
+    })
+    .always(function(data) {
+      $.each(data, function(i, data) {
+        $('.timeline__message-list').append($(buildHTML(data)));
+      });
+      $('.chat-area__timeline').animate({
+        scrollTop: $('.chat-area__timeline').get(0).scrollHeight}, 4000);
+    });
+  }
 });
