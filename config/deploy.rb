5ef402e0923eca01e500091f43c8c087d9aa787d
ars-ishi <ars.ishi11@gmail.com>
2018-10-01 21:05:48 +0900

    Edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,6 @@ lock "~> 3.11.0"
 
 set :application, "chat-space"
 set :repo_url, 'git@github.com:ars-ishi/chat-space.git'
-set :linked_files, %w{ config/secrets.yml }
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -23,6 +22,8 @@ set :default_env, {
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
+
+set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
